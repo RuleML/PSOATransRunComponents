bef192425135e56cef1049e5b38e93766b3863aa
reuben.peterpaul <reuben.peterpaul@gmail.com>
2012-02-22 19:12:56 +0000

    fixed broken dependency

--- RIF_BLD_Parser/docs/BLD_files/index.php
+++
@@ -1,8 +0,0 @@
-/* generated javascript */
-var skin = 'w3cmonobook';
-var stylepath = '/2005/rules/wiki/skins';
-
-/* MediaWiki:Common.js */
-/* Any JavaScript here will be loaded for all users on every page load. */
-
-/* MediaWiki:W3cmonobook.js (deprecated; migrate to Common.js!) */
