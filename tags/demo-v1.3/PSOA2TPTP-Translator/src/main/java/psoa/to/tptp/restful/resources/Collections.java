27db4b28bda63cd441c78c60ca7cf132f76f0721
reuben.peterpaul@gmail.com <reuben.peterpaul@gmail.com@8a223785-3c0b-77d7-c87c-ee6d03a7c174>
2012-05-04 04:08:39 +0000

    Tagging latest release.

---
+++ tags/demo-v1.3/PSOA2TPTP-Translator/src/main/java/psoa/to/tptp/restful/resources/Collections.java
@@ -0,0 +1,46 @@
+package psoa.to.tptp.restful.resources;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+enum Collections {
+;
+
+	protected static <T> List<T> list() {
+		return new ArrayList<T>();
+	}
+	
+	protected static <T> List<T> list(Collection<T> c) {
+		return new ArrayList<T>(c);
+	}
+	
+	protected static <T> Set<T> set() {
+		return new HashSet<T>();
+	}
+	
+	protected static <K,V> Map<K,V> map() {
+		return new HashMap<K, V>();
+	}
+	
+	protected static <K,V> boolean has(Map<K,V> m, K k) {
+		return m.containsKey(k);
+	}
+	
+	protected static <T> T first(Collection<T> coll) {
+		try {
+			return coll.iterator().next();
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+	}
+	
+	protected static <K,V> V get(Map<K,V> m, K k) {
+		return m.get(k);
+	}
+}
