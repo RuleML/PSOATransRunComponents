9998ae434d25724fe5cc2192ddf6a9a55d8ea3cf
MitsiCash <mitsikas.th@gmail.com>
2020-09-18 20:55:59 +0300

    update Semantic Version, including in pom files

--- PSOATransRun/src/main/java/org/ruleml/psoa/psoatransrun/PSOATransRun.java
+++ PSOATransRun/src/main/java/org/ruleml/psoa/psoatransrun/PSOATransRun.java
@@ -107,7 +107,7 @@ public class PSOATransRun {
 		{
 			systemInstantiation = "[PSOA2TPTP,VampirePrime]";
 		}
-		return "PSOATransRun" + "1.4.5" + systemInstantiation;
+		return "PSOATransRun" + "1.5" + systemInstantiation;
 	}
 	
 	/***
